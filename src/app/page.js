a44030707f43cb3e2f5d86faf38d0799262e074e
Sadgruru <sadgurustreak@gmail.com>
2025-10-14 00:08:12 +0530

    Second 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Showcase from "@/components/Showcase";
-import FloatingCapsule from "@/components/FloatingCapsule";
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
-      <FloatingCapsule />
       <Hero />
       <About />
       <Showcase />
